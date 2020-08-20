601c43e2700aad24dff6243e539a32e44fde38e4
Murashov_AA <a@ya.ru>
2020-08-20 13:12:20 +0400

    removed test data

--- src/main/java/com/example/demosite/repository/TestDataRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demosite.repository;
-
-import com.example.demosite.model.TestData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestDataRepository extends JpaRepository<TestData, Long>{
-}
