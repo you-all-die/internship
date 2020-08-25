d8a22a768fd9a1694243a757ea6c16386bab751c
Сергей Лапшин <gustav474@gmail.com>
2020-08-26 03:10:25 +0400

    Исправлены сущности Category, Product, ProductStatus. Обновлен порядок создания и хранения changelog'ов

--- src/main/java/com/example/demosite/entity/Product.java
+++ src/main/java/com/example/demosite/entity/Product.java
@@ -11,13 +11,19 @@ import java.util.Set;
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
+
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
+
     private String name;
+
     private String description;
+
     private String picture;
+
     @ManyToOne
     @JoinColumn(name = "status_id")
     private ProductStatus status;
