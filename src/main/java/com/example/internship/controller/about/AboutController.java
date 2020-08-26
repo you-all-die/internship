c66602f3647912852580af23e14f70e801e7aa7f
Юрий Самохвалов <samyual@gmail.com>
2020-08-26 22:23:35 +0300

    В TODO написано, что переделать. Спать!

--- src/main/java/com/example/internship/controller/about/AboutController.java
+++ src/main/java/com/example/internship/controller/about/AboutController.java
@@ -1,15 +1,23 @@
 package com.example.internship.controller.about;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/about")
+@Slf4j
 public class AboutController {
 
+    @Value("${yandex.maps.apikey}")
+    private String yandexMapsApiKey;
+
     @GetMapping("")
-    public String showAboutPage() {
+    public String showAboutPage(Model model) {
+        model.addAttribute("apikey", yandexMapsApiKey);
         return "about/index";
     }
 }
