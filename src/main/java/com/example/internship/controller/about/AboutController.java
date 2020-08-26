3ede82029a77749ccb7b012d1f33a1bedac32407
Юрий Самохвалов <samyual@gmail.com>
2020-08-26 22:03:56 +0300

    Заготовки контроллера About

--- src/main/java/com/example/internship/controller/about/AboutController.java
+++ src/main/java/com/example/internship/controller/about/AboutController.java
@@ -1,15 +1,15 @@
-package com.example.internship.controller.home;
+package com.example.internship.controller.about;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
-public class HomeControllers {
+@RequestMapping("/about")
+public class AboutController {
 
-    @GetMapping
-    public String homePage() {
-        return "home/index";
+    @GetMapping("")
+    public String showAboutPage() {
+        return "about/index";
     }
 }
