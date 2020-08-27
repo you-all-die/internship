3d34690ea923a7b89ea8c77fb94ed9c300142db3
Murashov_AA <a@ya.ru>
2020-08-27 14:11:59 +0400

    admin page fix

--- src/main/java/com/example/internship/controller/admin/AdminController.java
+++ src/main/java/com/example/internship/controller/admin/AdminController.java
@@ -3,6 +3,7 @@ package com.example.internship.controller.admin;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -10,15 +11,6 @@ public class AdminController {
 
     @GetMapping("/admin")
     public String urlAdmin (Model model) {
-        return "admin";
+        return "admin/admin";
     }
-    //@GetMapping("/admin/products")
-    //public String controlProduct (Model model) {
-        //return "control-product";
-    //}
-    //@GetMapping("/control-category")
-    //public String controlCategory (Model model) {
-        //return "control-category";
-    //}
-
 }
