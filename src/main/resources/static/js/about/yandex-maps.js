04cc7da7ee3a9d9f892f1a53cb7a4a073334042d
Юрий Самохвалов <samyual@gmail.com>
2020-08-28 14:47:33 +0300

    Увеличен зум, добавлена функция setPlacemark

--- src/main/resources/static/js/about/yandex-maps.js
+++ src/main/resources/static/js/about/yandex-maps.js
@@ -3,11 +3,15 @@ var map;
 ymaps.ready(function () {
     map = new ymaps.Map('map', {
         center: [54.314192, 48.403123],
-        zoom: 10
+        zoom: 16
     });
 });
 
-ymaps.markAndGo = function(outlet) {
-    var outletMark = new ymaps.Placemark([outlet.longitude, outlet.latitude]);
+ymaps.setPlacemark = function (outlet) {
+    var outletPlacemark = new ymaps.Placemark([outlet.longitude, outlet.latitude]);
+    map.getObjects.add(outletPlacemark);
+}
+
+ymaps.showOutlet = function(outlet) {
     map.panTo([outlet.longitude, outlet.latitude], {duration: 2000});
 }
