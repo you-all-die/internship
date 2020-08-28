80f93f2e307786e46a2b5bb709b9f5d1d8454974
Roman <minnesota.git@gmail.com>
2020-08-28 14:55:38 +0300

    Product Page vol .1
    не могу добавить картинку (

---
+++ src/main/java/com/example/internship/controller/product/ShowProductController.java
@@ -0,0 +1,26 @@
+package com.example.internship.controller.product;
+
+
+import com.example.internship.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/products")
+@AllArgsConstructor
+public class ShowProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/{id}")
+    public String showProduct(@PathVariable("id") long id, Model model){
+        model.addAttribute("product",
+                productService.getProductById(id));
+
+        return "product/product";
+    }
+}
