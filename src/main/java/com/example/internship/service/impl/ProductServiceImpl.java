3c002dddcbc666e147e45af2330b398fa312e38e
Danil Modenov <jack.white0011@gmail.com>
2020-08-28 06:50:03 +0400

    1) Add ModelMapperConfig to convert model to dto and vice versa. 2) Create ProductController, which returns the product page. 3) Create Dto: ProductDto, ProductStatusDto, CategoryDto. 4) Update ProductRepository -> replacing CrudRepository with JpaRepository. 5) Division of responsibility for ProductService, as well as adding the implementation of this service. 6) All methods in the ProductService are now get and return dto, need to update the product display from the admin page. 7) Create front page products.html and add link on products.html to main header. 8) Add dependency ModelMapper and ThymeleafSpringSecurity

---
+++ src/main/java/com/example/internship/service/impl/ProductServiceImpl.java
@@ -0,0 +1,52 @@
+package com.example.internship.service.impl;
+
+import com.example.internship.dto.ProductDto;
+import com.example.internship.entity.Product;
+import com.example.internship.repository.ProductRepository;
+import com.example.internship.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepo;
+
+    private final ModelMapper mapper;
+
+    @Override
+    public List<ProductDto> findAll() {
+        return productRepo.findAll().stream().map(this::convertToDto).collect(Collectors.toList());
+    }
+
+    @Override
+    public void removeProduct(Long id) {
+        productRepo.deleteById(id);
+    }
+
+    @Override
+    public void addProduct(ProductDto productDto) {
+        productRepo.save(convertToModel(productDto));
+    }
+
+    @Override
+    public List<ProductDto> findByName(String name) {
+        return productRepo.findByNameContainsIgnoreCase(name)
+                .stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+
+    private ProductDto convertToDto(Product product) {
+        return mapper.map(product, ProductDto.class);
+    }
+
+    private Product convertToModel(ProductDto productDto) {
+        return mapper.map(productDto, Product.class);
+    }
+}
