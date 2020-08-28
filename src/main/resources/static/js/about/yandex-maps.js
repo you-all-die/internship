22901f4c5aa7dd4afde8bb0118886604f4f97ade
Юрий Самохвалов <samyual@gmail.com>
2020-08-28 08:08:42 +0300

    Попытка подключения js к списку магазин

--- src/main/resources/static/js/about/yandex-maps.js
+++ src/main/resources/static/js/about/yandex-maps.js
@@ -1,6 +1,13 @@
+var map;
+
 ymaps.ready(function () {
-    var map = new ymaps.Map('map', {
+    map = new ymaps.Map('map', {
         center: [54.314192, 48.403123],
         zoom: 10
     });
 });
+
+ymaps.markAndGo = function(outlet) {
+    var outletMark = new ymaps.Placemark([outlet.longitude, outlet.latitude]);
+    map.panTo([outlet.longitude, outlet.latitude], {duration: 2000});
+}
