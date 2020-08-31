f59d232cbf9b5970a0734cd2fe522693cafa5e1c
Сергей Лапшин <gustav474@gmail.com>
2020-08-31 18:17:18 +0400

    Добавид CustomerRepository

--- src/main/java/com/example/internship/repository/CustomerRepository.java
+++ src/main/java/com/example/internship/repository/CustomerRepository.java
@@ -3,6 +3,6 @@ package com.example.internship.repository;
 import com.example.internship.entity.Customer;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
+public  interface CustomerRepository extends CrudRepository<Customer, Long> {
     public Customer findByEmail(String email);
 }
