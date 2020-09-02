f477cdca344ea774a9c9661c6c7cceec932958c9
Юрий Самохвалов <samyual@gmail.com>
2020-09-02 22:48:36 +0300

    Отметил устаревающие методы в интерфейсе ProductService

--- src/main/java/com/example/internship/service/ProductService.java
+++ src/main/java/com/example/internship/service/ProductService.java
@@ -1,29 +1,41 @@
 package com.example.internship.service;
 
-import com.example.internship.dto.ProductDto;
-import com.example.internship.dto.ProductSearchResult;
+import com.example.internship.dto.product.ProductDto;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
-
 public interface ProductService {
-    List<ProductDto> findAll();
-    void removeProduct(Long id);
-    void addProduct(ProductDto productDto);
-    List<ProductDto> findByName(String name);
+    @Deprecated
+    List<com.example.internship.dto.ProductDto> findAll();
 
-    List<com.example.internship.dto.product.ProductDto.Response.Full> getAll();
+    @Deprecated
+    void removeProduct(Long id);
+
+    @Deprecated
+    void addProduct(com.example.internship.dto.ProductDto productDto);
+
+    @Deprecated
+    List<com.example.internship.dto.ProductDto> findByName(String name);
+
+    List<ProductDto.Response.Full> getAll();
+
+    Optional<ProductDto.Response.Full> getById(Long id);
+
     void delete(Long id);
-    void save(com.example.internship.dto.product.ProductDto.Response.Full productDto);
-    List<com.example.internship.dto.product.ProductDto.Response.Full> getByName(String name);
-    Optional<com.example.internship.dto.product.ProductDto.Response.Full> getById(Long id);
-    ProductDto getProductById(Long id);
+
+    void save(ProductDto.Response.Full productDto);
+
+    List<ProductDto.Response.Full> getByName(String name);
+
+    com.example.internship.dto.ProductDto getProductById(Long id);
 
     // Продукт по id
-    Optional<ProductDto> findById(Long id);
+    @Deprecated
+    Optional<com.example.internship.dto.ProductDto> findById(Long id);
+
     // Поиск по условиям
-    ProductSearchResult search(Optional<String> name, Optional<Long> categoryId, Optional<BigDecimal> priceFrom,
+    com.example.internship.dto.ProductSearchResult search(Optional<String> name, Optional<Long> categoryId, Optional<BigDecimal> priceFrom,
                                Optional<BigDecimal> priceTo, Integer pageSize, Integer pageNumber);
 }
