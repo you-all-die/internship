08bbf9ce9191ec69d252af2dbfc4321de3a94a06
Romodin Aleksey <workbegin@gmail.com>
2020-09-03 02:04:37 +0400

    Admin: список товаров, редактирование, добавление, удаление.

--- src/main/java/com/example/internship/controller/admin/AdminProductsController.java
+++ src/main/java/com/example/internship/controller/admin/AdminProductsController.java
@@ -1,61 +1,139 @@
 package com.example.internship.controller.admin;
 
-import com.example.internship.dto.ProductDto;
+
+import com.example.internship.entity.Product;
+import com.example.internship.service.CategoryService;
 import com.example.internship.service.ProductService;
+import com.example.internship.service.ProductStatusService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/admin")
 @AllArgsConstructor
 public class AdminProductsController {
     private final ProductService productService;
+    private final CategoryService categoryService;
+    private final ProductStatusService productStatusService;
+    private static final String DEFAULT_FILE_PICTURE_NAME = "default.jpg";
 
-    @GetMapping({"/products"})
-    public String saveDataForm(Model model, @RequestParam(value = "name", required = false) String productName) {
-        List<ProductDto> list;
-        if (productName == null) {
-            list = productService.findAll();
-        } else {
-            list = productService.findByName(productName);
+    //Генерация имени файла
+    private String generationFileName(Product product, MultipartFile pictureName){
+        String filePictureName;
+        String fileExtension;
+
+        fileExtension = pictureName.getOriginalFilename();
+        int index = fileExtension.lastIndexOf('.');
+        fileExtension = fileExtension.substring(index);
+        filePictureName = product.getId() + "_" + product.getCategory().getId() + fileExtension;
+        return filePictureName;
+    }
+
+
+    @GetMapping(value="/products")
+    public String findProduct(@RequestParam(value = "search", required = false) String searchName, Model model) {
+        if (searchName==null){
+            model.addAttribute("products", productService.findAll());
         }
-        model.addAttribute("productList", list);
+        else{
+            model.addAttribute("products", productService.findByName(searchName));
+        }
         return "/admin/products";
     }
 
     @PostMapping(value="/products")
-    public String findProduct(@RequestParam("name") String name, Model model) {
-        return "redirect:/admin/products?name=" + name;
+    public String searchProduct(@RequestParam("search") String searchName, Model model) {
+        return "redirect:/admin/products?search=" + searchName;
     }
 
+    //Удаление товара
     @PostMapping(value="/product/delete")
     public String deleteProduct(@RequestParam("productId") Long id, Model model) {
-        //здесь должны быть проверки на возможность удаления продукта, а пока просто удаляем
         productService.removeProduct(id);
+        //здесь должны быть проверки на возможность удаления продукта, а пока просто удаляем
         return "redirect:/admin/products";
+    }
+
+    //Форма редактирования существующего продукта
+    @GetMapping(value="/product/edit")
+    public String editProduct(@RequestParam(value = "productId") Long id, Model model) {
+        Product product = productService.findByIdProduct(id);
+        model.addAttribute("product",product);
+        model.addAttribute("product_status", productStatusService.findAll());
+        model.addAttribute("selectcategory", categoryService.findAllSortById());
+        return "admin/productsave";
     }
 
     @PostMapping(value="/product/edit")
-    public String editProduct(@RequestParam("productId") Long id, Model model) {
-        return "redirect:/admin/product/edit?id=" + id;
+    public String editProduct(@RequestParam("productId") String id, Model model) {
+        return "redirect:/admin/product/edit?productId=" + id;
     }
 
-    @PostMapping(value="/product/hide")
-    public String hideProduct(@RequestParam("productId") Long id, Model model) {
-        //productService.hideProduct(id);
+    //Форма добавления нового продукта
+    @GetMapping(value="/add")
+    public String addNewProductForm(Model model) {
+        Product product = new Product();
+        model.addAttribute("product", product);
+        model.addAttribute("product_status", productStatusService.findAll());
+        model.addAttribute("selectcategory", categoryService.findAllSortById());
+        return "admin/productsave";
+    }
+
+    //Общий метод на добавление и редактирование товара
+    @PostMapping(value="/product/save")
+    public String saveProduct(@ModelAttribute("product") Product product,
+                              @RequestParam("picture_file") MultipartFile pictureNew,
+                              Model model) throws IOException {
+
+        String filePictureName = DEFAULT_FILE_PICTURE_NAME;
+        String defaultPictureName = DEFAULT_FILE_PICTURE_NAME;
+        Boolean renamePicture = false;
+        String uploadPath = System.getProperty("user.dir") + "/img_product/";
+
+        //Если ID есть->редактирование, иначе добавление нового товара
+        //Генерация имени файла
+        //Удаление старого файла из папки
+        if (product.getId() != null){
+            if (pictureNew.getSize() > 0){
+                filePictureName = generationFileName(product, pictureNew);
+                if (!product.getPicture().equals(defaultPictureName)){
+                    File oldPicture = new File(uploadPath + product.getPicture());
+                    if(oldPicture.exists()) {
+                        oldPicture.delete();
+                    }
+                }
+            }
+            else filePictureName = product.getPicture();
+        }
+        else {
+            renamePicture = true;
+        }
+
+        product.setPicture(filePictureName);
+        productService.saveProduct(product);
+
+        //Переименование названия фото в БД
+        if (renamePicture & (pictureNew.getSize() > 0)){
+            filePictureName = generationFileName(product,pictureNew);
+            product.setPicture(filePictureName);
+            productService.saveProduct(product);
+        }
+
+        //Cоздание директории /img_product/, сохранение файла
+        if (pictureNew.getSize() > 0) {
+            File imgDir = new File(uploadPath);
+            if (!imgDir.exists()) {
+                imgDir.mkdir();
+            }
+            pictureNew.transferTo(new File(uploadPath + filePictureName));
+        }
         return "redirect:/admin/products";
     }
-
-    @PostMapping(value="/product/add")
-    public String addProduct(Model model) {
-        return "redirect:/admin/product/add";
-    }
 }
+
