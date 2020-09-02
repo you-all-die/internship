45fb301f8bf1dca447d924355f158839f3083b07
Ivan Gubanov <lucky.javanni@yandex.ru>
2020-09-02 17:49:37 +0400

    Merge

--- src/main/java/com/example/internship/repository/ProductRepository.java
+++ src/main/java/com/example/internship/repository/ProductRepository.java
@@ -3,7 +3,6 @@ package com.example.internship.repository;
 import com.example.internship.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
