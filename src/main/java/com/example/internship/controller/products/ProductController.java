7ce1da7d8a3a42f085ab05fdb2e10808039a636d
Юрий Самохвалов <samyual@gmail.com>
2020-09-04 20:40:56 +0300

    Перевод главного шаблона

--- src/main/java/com/example/internship/controller/products/ProductController.java
+++ src/main/java/com/example/internship/controller/products/ProductController.java
@@ -1,39 +1,42 @@
 package com.example.internship.controller.products;
 
-import com.example.internship.dto.ProductDto;
+import com.example.internship.service.GsProductService;
 import com.example.internship.service.ProductService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
 @Controller
-@RequestMapping("/")
-@AllArgsConstructor
+@RequestMapping("/product") // Вынес /product в маппинг(СЮА)
+@RequiredArgsConstructor // Заменил All на Required (СЮА)
 public class ProductController {
 
     private final ProductService productService;
+    private final GsProductService gsProductService;
 
-    @GetMapping("products")
+/*  Убрал, так как реализую свой функционал (СЮА)
+    @GetMapping("")
     public String getProducts(Model model) {
         List<ProductDto> products = productService.findAll();
         model.addAttribute("products", products);
         return "products/products";
     }
+*/
 
     /**
      * @Author Роман Каравашкин
      */
-    @GetMapping("product/{id}")
+    @GetMapping("/{id}") // убрал ненужный /product (СЮА)
     public String showProduct(@PathVariable("id") long id, Model model) {
-        model.addAttribute("product",
-                productService.getProductById(id));
-
+        model.addAttribute("product", productService.getProductById(id));
         return "products/product";
+    }
 
+    @GetMapping("")
+    public String showProducts() {
+        return "product/index";
     }
 }
