7127db439e5fd5626999a42ad82bb69d4605e5c3
Юрий Самохвалов <samyual@gmail.com>
2020-09-04 03:17:49 +0300

    Новая реализация получения популярных товаров

--- src/main/java/com/example/internship/repository/ProductRepository.java
+++ src/main/java/com/example/internship/repository/ProductRepository.java
@@ -3,6 +3,8 @@ package com.example.internship.repository;
 import com.example.internship.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,7 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
 
     List<Product> findByNameContainsIgnoreCase(String name);
+
+    @Query("select * from Product limit :limit")
+    List<Product> findPopular(@Param("limit") long limit);
 }
