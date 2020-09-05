e3bf3a2628f80fc9fed561672b936dee85a30a36
Юрий Самохвалов <samyual@gmail.com>
2020-09-05 11:03:20 +0300

    Набросок обработчика события onchange селектора категорий

--- src/main/resources/static/js/product/utils.js
+++ src/main/resources/static/js/product/utils.js
@@ -1,5 +1,5 @@
 const addToCart = function (productId) {
-    $.post({ url: '/product/cart?productId=' + productId })
+    $.post({ url: '/product/cart?productId=' + unescape(productId) })
         .done(function () {
             console.log('Успешно добавлен в корзину товар id=' + productId);
         })
@@ -7,4 +7,15 @@ const addToCart = function (productId) {
             console.log('Ошибка добавления в корзину товара id=' + productId);
         })
         .always(function () {});
+}
+
+const onCategoryChange = function (select) {
+    let category = select.options[select.selectedIndex].value;
+    $.post({ url: '' })
+        .done(function (products) {
+            console.log('Сервер вернул: ' + products);
+        })
+        .fail(function (error) {
+            console.log('Ошибка обращения к серверу: ' + error);
+        })
 }
