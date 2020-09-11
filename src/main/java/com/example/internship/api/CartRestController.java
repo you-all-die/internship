93032ae6c49888285a9d04fdf9337dfc4cf7cf23
Danil Modenov <jack.white0011@gmail.com>
2020-09-11 11:41:20 +0400

    Rest api cart fix

--- src/main/java/com/example/internship/api/CartRestController.java
+++ src/main/java/com/example/internship/api/CartRestController.java
@@ -1,12 +1,12 @@
 package com.example.internship.api;
 
-import com.example.internship.dto.OrderLineForm;
+import com.example.internship.api.dto.OrderLineForm;
 import com.example.internship.dto.OrderLineDto;
 import com.example.internship.service.CartService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,40 +28,41 @@ public class CartRestController {
     public ResponseEntity<List<OrderLineDto>> findAll(@PathVariable("customerId") Long customerId) {
         List<OrderLineDto> orderLines = cartService.findAll(customerId);
 
-        return orderLines.isEmpty() ? new ResponseEntity<>(HttpStatus.NOT_FOUND) :
-                new ResponseEntity<>(orderLines, HttpStatus.OK);
+        return orderLines.isEmpty() ? ResponseEntity.notFound().build() :
+                ResponseEntity.ok(orderLines);
     }
 
     @PutMapping("{customerId}/add/{productId}")
     @ApiOperation(value = "Добавляет товар в корзину.")
     public ResponseEntity<?> add(@PathVariable Long customerId, @PathVariable Long productId) {
 
-        return cartService.add(productId, customerId) ? new ResponseEntity<>(HttpStatus.OK) :
-                new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        return cartService.add(productId, customerId) ? ResponseEntity.ok().build() :
+                ResponseEntity.badRequest().build();
     }
 
     @PostMapping("{customerId}")
     @ApiOperation(value = "Обновляет количество товара в корзине.")
-    public ResponseEntity<?> updateQuantity(@PathVariable Long customerId, @RequestBody OrderLineForm orderLineForm) {
+    public ResponseEntity<?> updateQuantity(@PathVariable @ApiParam(value = "id пользователя.") Long customerId,
+                                            @RequestBody @ApiParam(value = "Форма заполнения продукта и его количества.") OrderLineForm orderLineForm) {
 
         return cartService.updateQuantity(orderLineForm.getProductId(),
-                orderLineForm.getProductQuantity(), customerId) ? new ResponseEntity<>(HttpStatus.OK) :
-                new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+                orderLineForm.getProductQuantity(), customerId) ? ResponseEntity.ok().build() :
+                ResponseEntity.badRequest().build();
     }
 
     @DeleteMapping("{customerId}/delete/{productId}")
     @ApiOperation(value = "Удаляет один товар из корзины.")
     public ResponseEntity<?> remove(@PathVariable Long customerId, @PathVariable Long productId) {
 
-        return cartService.remove(productId, customerId) ? new ResponseEntity<>(HttpStatus.OK) :
-                new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        return cartService.remove(productId, customerId) ? ResponseEntity.ok().build() :
+                ResponseEntity.badRequest().build();
     }
 
     @DeleteMapping("{customerId}")
     @ApiOperation(value = "Удаляет все товары в корзине.")
     public ResponseEntity<?> removeAll(@PathVariable Long customerId) {
 
-        return cartService.removeAll(customerId) ? new ResponseEntity<>(HttpStatus.OK) :
-                new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        return cartService.removeAll(customerId) ? ResponseEntity.ok().build() :
+                ResponseEntity.badRequest().build();
     }
 }
