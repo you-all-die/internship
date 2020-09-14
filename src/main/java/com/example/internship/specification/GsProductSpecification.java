cf8a79e66a51672935dcea21985a033431662ca8
Юрий Самохвалов <samyual@gmail.com>
2020-09-14 13:20:53 +0300

    Переписал спецификацию, теперь работает правильно

--- src/main/java/com/example/internship/specification/GsProductSpecification.java
+++ src/main/java/com/example/internship/specification/GsProductSpecification.java
@@ -1,10 +1,15 @@
 package com.example.internship.specification;
 
 import com.example.internship.entity.Product;
-import com.example.internship.helper.SpecificationHelper;
+import com.example.internship.helper.PredicateHelper;
 import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.domain.Specification;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.LinkedList;
@@ -13,51 +18,74 @@ import java.util.List;
 /**
  * @author Самохвалов Юрий Алексеевич
  */
-public enum GsProductSpecification {
-    ;
+public class GsProductSpecification implements Specification<Product> {
 
-    private static Specification<Product> withNameLike(final String searchString) {
-        return (Specification<Product>) (product, query, builder) ->
-                builder.like(builder.lower(product.get("name")), SpecificationHelper.getLikePattern(searchString));
+    private final String searchString;
+    private final Collection<Long> categoryIds;
+    private final BigDecimal lowerLimit;
+    private final BigDecimal upperLimit;
+
+    private GsProductSpecification(
+            Builder builder
+    ) {
+        this.searchString = builder.searchString;
+        this.categoryIds = builder.categoryIds;
+        this.lowerLimit = builder.lowerLimit;
+        this.upperLimit = builder.upperLimit;
     }
 
-    private static Specification<Product> ofCategories(Collection<Long> categoryIds) {
-        return (Specification<Product>) (product, query, builder) ->
-                builder.in(product.get("category").get("id")).value(categoryIds);
-    }
+    @Override
+    public Predicate toPredicate(
+            @NotNull Root<Product> product,
+            @NotNull CriteriaQuery<?> query,
+            @NotNull CriteriaBuilder builder
+    ) {
+        final List<Predicate> predicates = new LinkedList<>();
 
-    private static Specification<Product> priceBetween(BigDecimal lowerLimit, BigDecimal upperLimit) {
-        return (Specification<Product>) (product, query, builder) ->
-                builder.between(product.get("price").as(BigDecimal.class), lowerLimit, upperLimit);
+        if (null != searchString && !searchString.isEmpty()) {
+            predicates.add(builder.like(builder.lower(product.get("name")), PredicateHelper.getLikePattern(searchString)));
+        }
+
+        if (null != categoryIds && !categoryIds.isEmpty()) {
+            predicates.add(builder.in(product.get("category").get("id")).value(categoryIds));
+        }
+
+        if (null != lowerLimit && null != upperLimit) {
+            predicates.add(builder.between(product.get("price").as(BigDecimal.class), lowerLimit, upperLimit));
+        } else if (null != lowerLimit) {
+            predicates.add(builder.greaterThanOrEqualTo(product.get("price"), lowerLimit));
+        } else {
+            predicates.add(builder.lessThanOrEqualTo(product.get("price").as(BigDecimal.class), upperLimit));
+        }
+
+        return builder.and(predicates.toArray(new Predicate[]{}));
     }
 
     @NoArgsConstructor
     public static class Builder {
-        private final List<Specification<Product>> specifications = new LinkedList<>();
+        private String searchString;
+        private Collection<Long> categoryIds;
+        private BigDecimal lowerLimit;
+        private BigDecimal upperLimit;
 
         public Builder nameLike(String searchingString) {
-            if (null != searchingString && !searchingString.isBlank()) {
-                specifications.add(GsProductSpecification.withNameLike(searchingString));
-            }
+            this.searchString = searchingString;
             return this;
         }
 
         public Builder ofCategories(List<Long> categoryIds) {
-            if (null != categoryIds && !categoryIds.isEmpty()) {
-                specifications.add(GsProductSpecification.ofCategories(categoryIds));
-            }
+            this.categoryIds = categoryIds;
             return this;
         }
 
-        public Builder priceBetween(BigDecimal lowerPriceLimit, BigDecimal upperPriceLimit) {
-            if (null != lowerPriceLimit && null != upperPriceLimit) {
-                specifications.add(GsProductSpecification.priceBetween(lowerPriceLimit, upperPriceLimit));
-            }
+        public Builder priceBetween(BigDecimal lowerLimit, BigDecimal upperLimit) {
+            this.lowerLimit = lowerLimit;
+            this.upperLimit = upperLimit;
             return this;
         }
 
         public Specification<Product> build() {
-            return SpecificationHelper.combineAll(specifications);
+            return new GsProductSpecification(this);
         }
     }
 }
