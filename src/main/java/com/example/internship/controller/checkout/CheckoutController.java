77ad1fab156c77c3c38c67652ce164dc77bdd428
Сергей Лапшин <gustav474@gmail.com>
2020-09-15 13:22:23 +0400

    fix develop with CheckoutController

--- src/main/java/com/example/internship/controller/checkout/CheckoutController.java
+++ src/main/java/com/example/internship/controller/checkout/CheckoutController.java
@@ -1,24 +1,14 @@
 package com.example.internship.controller.checkout;
 
 import com.example.internship.dto.CustomerDto;
-import com.example.internship.dto.OrderDto;
-import com.example.internship.dto.OrderLineDto;
 import com.example.internship.entity.Cart;
 import com.example.internship.entity.Customer;
-import com.example.internship.entity.Product;
-import com.example.internship.service.CartService;
-import com.example.internship.service.CheckoutService;
 import com.example.internship.service.impl.CustomerServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import java.math.BigDecimal;
-import java.util.List;
 import java.util.Optional;
 
 /**
