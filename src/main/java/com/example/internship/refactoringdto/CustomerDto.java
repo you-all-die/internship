281f1338059492b9ec2ba09927ce5aa632cf3272
Danil Modenov <jack.white0011@gmail.com>
2020-09-24 17:08:50 +0400

    Add view

--- src/main/java/com/example/internship/refactoringdto/CustomerDto.java
+++ src/main/java/com/example/internship/refactoringdto/CustomerDto.java
@@ -1,5 +1,6 @@
 package com.example.internship.refactoringdto;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -7,13 +8,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Set;
 
 /**
  * @author Ivan Gubanov
  */
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -22,36 +23,44 @@ import java.util.Set;
 public class CustomerDto {
 
     @Schema(description = "id покупателя")
+    @JsonView(View.All.class)
     private Long id;
 
     @Schema(description = "Имя")
     @Size(max = 64)
+    @JsonView(View.NoId.class)
     private String firstName;
 
     @Schema(description = "Отчество")
     @Size(max = 64)
+    @JsonView(View.NoId.class)
     private String middleName;
 
     @Schema(description = "Фамилия")
     @Size(max = 64)
+    @JsonView(View.NoId.class)
     private String lastName;
 
     @Schema(description = "Пароль")
     @Size(max = 60)
+    @JsonView(View.NoId.class)
     private String password;
 
     @Schema(description = "Телефон")
     @Size(max = 64)
+    @JsonView(View.NoId.class)
     private String phone;
 
     @Schema(description = "Почта")
     @Size(max = 64)
+    @JsonView(View.NoId.class)
     private String email;
 
     @Schema(description = "Список адресов")
+    @JsonView(View.NoId.class)
     private Set<AddressDto> addresses;
 
     @Schema(description = "Корзина")
-    @NotNull
+    @JsonView({View.NoId.class, View.All.class})
     private CartDto cart;
 }
---
+++ src/main/java/com/example/internship/refactoringdto/View.java
@@ -0,0 +1,11 @@
+package com.example.internship.refactoringdto;
+
+/**
+ * @author Modenov D.A
+ */
+public class View {
+
+    public interface NoId {}
+
+    public interface All extends NoId {}
+}
