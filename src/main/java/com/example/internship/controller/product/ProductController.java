7fd32273fede2c8c272e5e9773cc6c8829715a7a
Romodin Aleksey <workbegin@gmail.com>
2020-09-26 12:57:02 +0400

    Обновление шаблона товара. Шаблон для отображения отзывов.

--- src/main/java/com/example/internship/controller/product/ProductController.java
+++ src/main/java/com/example/internship/controller/product/ProductController.java
@@ -1,18 +1,22 @@
 package com.example.internship.controller.product;
 
+import com.example.internship.dto.CustomerDto;
+import com.example.internship.dto.category.CategoryDto;
 import com.example.internship.dto.product.SearchResult;
 import com.example.internship.service.ProductService;
+import com.example.internship.service.category.GsCategoryService;
+import com.example.internship.service.customer.CustomerService;
 import com.example.internship.service.product.GsProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
 
 /**
@@ -28,15 +32,47 @@ public class ProductController {
 
     private final ProductService productService;
     private final GsProductService gsProductService;
+    private final CustomerService customerService;
+    private final GsCategoryService categoryService;
 
     /**
      * @author Роман Каравашкин
      */
-    @GetMapping("/{id}") // убрал ненужный /product (СЮА)
-    public String showProduct(@PathVariable("id") long id, Model model) {
+    @GetMapping("/{id}")
+    public String showProduct(@PathVariable("id") long id, Authentication authentication, Model model) {
+
+        //Получаем дерево предков категорий
+        final List<CategoryDto.Response.All> ancestors =
+                categoryService.findAncestors(productService.getProductById(id).getCategory().getId());
+        model.addAttribute("breadcrumb", ancestors);
+
+        /*Получаем пользователя из авторизации
+         * - Если пользователь авторизирован, будет оставлять отзывы от своего имени
+        */
+        Optional<CustomerDto> customer = customerService.getFromAuthentication(authentication);
+        customer.ifPresent(customerDto -> model.addAttribute("customerName", true));
+
         model.addAttribute("product", productService.getProductById(id));
         return "products/product";
     }
+
+    /**
+     * Добавление нового комментария к товару
+     *
+     * @param id код продукта
+     * @param customerName имя пользователя
+     * @param model модель
+     * @return переход на страницу текущего товара
+     */
+    @PostMapping("/{id}/addNewComment")
+    public String addNewCommentProduct(@PathVariable("id") Long id,
+                                       @RequestParam(value = "customerName", required = false) String customerName,
+                                       @RequestParam(value = "rating", required = false) Integer rating,
+                                       Model model){
+
+        return "redirect:" + BASE_URL + "/" + id;
+    }
+
 
     @GetMapping
     public String viewProductPage(
