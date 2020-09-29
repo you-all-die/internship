2c1f4ab5dbb67905b29cad0e3024eddb92156811
Ivan Gubanov <lucky.javanni@yandex.ru>
2020-09-29 11:04:07 +0400

    fix checksytle View dto

--- src/main/java/com/example/internship/refactoringdto/View.java
+++ src/main/java/com/example/internship/refactoringdto/View.java
@@ -9,21 +9,25 @@ public class View {
     /**
      * Устанавливается для основных полей, включая id.
      */
-    public interface Public {}
+    public interface Public {
+    }
 
     /**
      * Устанавливается для полей, которые можно редактировать.
      */
-    public interface Update {}
+    public interface Update {
+    }
 
     /**
      * Устанавливается для показа всех полей, за исключением Private полей.
      */
-    public interface All extends Public {}
+    public interface All extends Public {
+    }
 
     /**
      * Устанавливается для секретных полей, которые не стоит редактировать и/или видеть.
      */
-    public interface Private extends All {}
+    public interface Private extends All {
+    }
 
 }
