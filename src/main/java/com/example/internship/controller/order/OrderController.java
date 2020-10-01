ab12d1155e42dc546999980a995c294a5eb1fa6e
Юрий Самохвалов <samyual@gmail.com>
2020-10-01 14:24:47 +0300

    Первый шаг с данными покупателя - подготовка

--- src/main/java/com/example/internship/controller/order/OrderController.java
+++ src/main/java/com/example/internship/controller/order/OrderController.java
@@ -1,21 +1,38 @@
 package com.example.internship.controller.order;
 
+import com.example.internship.dto.CustomerDto;
+import com.example.internship.service.customer.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 @RequestMapping(OrderController.BASE_MAPPING)
+@RequiredArgsConstructor
 public class OrderController {
 
     public static final String BASE_MAPPING = "/order";
     private static final String TEMPLATE_DIR = "/order";
 
+    private final CustomerService customerService;
+
     /**
      * Покупатель регистрируется или входит в систему.
      */
     @GetMapping
-    public String enterToSystem() {
+    public String enterToSystem(
+            Authentication authentication,
+            Model model
+    ) {
+        if (authentication.isAuthenticated()) {
+            final Optional<CustomerDto> optionalCustomerDto = customerService.getFromAuthentication(authentication);
+            model.addAttribute("customer", optionalCustomerDto.orElse(new CustomerDto()));
+        }
         return TEMPLATE_DIR + "/enter";
     }
 
